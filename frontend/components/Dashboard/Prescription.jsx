f2b8381cf557cdd865d54960e9a3bd48d5877a56
Precious Michael <precious.michael2002@gmail.com>
2024-05-15 14:49:13 +0100

    uodated Prescription component

--- frontend/components/Dashboard/Prescription.jsx
+++ frontend/components/Dashboard/Prescription.jsx
@@ -17,21 +17,29 @@ import { ScrollArea } from '../ui/scroll-area'
 import { Circles } from 'react-loader-spinner'
 
 function Prescription() {
-  const [count, setCount] = useState(1)
+  const [count, setcount] = useState(1)
   const [NumberofDrugs, setNumberofDrugs] = useState(1)
   const [TypeofDrug, setTypeofDrug] = useState('tablet')
   const [SelectedSerialNumber, setSelectedSerialNumber] = useState()
+  //INfo
+
   const [email, setEmail] = useState('')
   const [PhoneNum, setPhoneNum] = useState('')
   const [Duration, setDuration] = useState(1)
+
+
+
+
   const [SerialNumber, setSerialNumber] = useState([])
-  const [fields, setFields] = useState([])
-  const [visibleGroups, setVisibleGroups] = useState([true])
-  const [loading, setLoading] = useState(false)
+
+  const [fields, setFields] = useState([]); // Initial fields
+  const [visibleGroups, setVisibleGroups] = useState([true]);
+
+  const [searching, setSearching] = useState(false)
 
   const newVisibleGroups = NumberofDrugs.length / 3 > visibleGroups.length
-    ? [...visibleGroups, false]
-    : visibleGroups.slice(0, NumberofDrugs.length / 3)
+      ? [...visibleGroups, false]
+      : visibleGroups.slice(0, NumberofDrugs.length / 3);
 
   const handleNumberOfDrug = (value) => {
     setNumberofDrugs(value)
@@ -44,44 +52,73 @@ function Prescription() {
   }
   const handleNextStep = async () => {
     if (count === 1) {
-      setLoading(true)
+      setSearching(true)
       await fetchSerial()
-      setLoading(false)
+      setSearching(false)
     }
-    setCount(count + 1)
+    setcount(count + 1)
     handleNumberField()
+    
   }
   const handlePrevStep = () => {
-    setCount(count - 1)
+    setcount(count - 1)
   }
+
   const handleNumberField = () => {
     if (NumberofDrugs === 1) {
-      setFields(['', '', ''])
+      setFields(['', '', '']);
     } else if (NumberofDrugs === 2) {
-      setFields(['', '', '', '', '', ''])
+      setFields(['', '', '', '', '', '']);
     }
     else if (NumberofDrugs === 3) {
-      setFields(['', '', '', '', '', '', '', '', ''])
+      setFields(['', '', '', '', '', '', '', '', '',]);
     }
-    setVisibleGroups(newVisibleGroups)
+    setVisibleGroups(newVisibleGroups);
   }
+
   const toggleVisibility = (index) => {
-    const newVisibleGroups = [...visibleGroups]
-    newVisibleGroups[index] = !newVisibleGroups[index]
-    setVisibleGroups(newVisibleGroups)
-  }
+    const newVisibleGroups = [...visibleGroups];
+    newVisibleGroups[index] = !newVisibleGroups[index];
+    setVisibleGroups(newVisibleGroups);
+  };
+
+  
   const handleFieldChange = (index, e) => {
-    const newFields = [...fields]
-    newFields[index] = e.target.value
-    setFields(newFields)
-  }
+    const newFields = [...fields];
+    newFields[index] = e.target.value;
+    setFields(newFields);
+  
+    
+  };
+  console.log(fields)
+
   const chunkArray = (array, size) => {
-    const chunkedArr = []
+    const chunkedArr = [];
     for (let i = 0; i < array.length; i += size) {
-      chunkedArr.push(array.slice(i, i + size))
+      chunkedArr.push(array.slice(i, i + size));
     }
-    return chunkedArr
-  }
+    return chunkedArr;
+  };
+
+  // useEffect(() => {
+  //   if(count === 2){
+  //     const fetchSerial = async () => {
+  //       const token = Cookies.get('user')
+  //       try {
+  //         const response = await fetch(`https://doseguardianapi.onrender.com/dispensers/search?layers=${NumberofDrugs}&drugType=${TypeofDrug}`, {
+  //           method: 'GET',
+  //           headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${token}` },
+  //         })
+  //         const data = await response.json()
+  //         setSerialNumber(data.dispensers)
+  //         console.log(data)
+  //       } catch (error) {
+  //         console.log(error)
+  //       }
+  //     }
+  //     fetchSerial()
+  //   }
+  // }, [])
   const fetchSerial = async () => {
     const token = Cookies.get('user')
     try {
@@ -90,27 +127,28 @@ function Prescription() {
         headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${token}` },
       })
       const data = await response.json()
-      setSerialNumber(data.dispensers || [])
+      setSerialNumber(data.dispensers)
+      console.log(SerialNumber)
     } catch (error) {
       console.log(error)
-      setSerialNumber([])
     }
   }
-
+ 
   return (
     <div className=''>
       <div className='flex flex-col gap-5 mt-10 h-[210px]'>
-        {count === 1 && (
+        {count == 1 && (
           <>
             <div className="grid w-full items-center gap-4">
-              <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="name" className="text-black text-lg">Number of Medications</Label>
-                <Select value={NumberofDrugs} onValueChange={handleNumberOfDrug}>
+          <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="name" className="text-black text-lg">Number of Medications</Label>
+              <Select  value={NumberofDrugs} onValueChange={handleNumberOfDrug}>
                   <SelectTrigger className="w-full">
                     <SelectValue placeholder="Select how many drugs" />
                   </SelectTrigger>
                   <SelectContent>
-                    <SelectGroup>
+                    <SelectGroup >
+                      {/* <SelectLabel>How many drug do you have to use?</SelectLabel> */}
                       <SelectItem value={1}>1</SelectItem>
                       <SelectItem value={2}>2</SelectItem>
                       <SelectItem value={3}>3</SelectItem>
@@ -119,122 +157,143 @@ function Prescription() {
                       <SelectItem value={6}>6</SelectItem>
                     </SelectGroup>
                   </SelectContent>
-                </Select>
-              </div>
-            </div>
-
-            <div className="grid w-full items-center gap-4">
-              <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="name" className="text-black text-lg">Drug Type</Label>
-                <Select value={TypeofDrug} onValueChange={handleTypeOfDrug}>
+               </Select>
+          </div>
+          </div>
+  
+          <div className="grid w-full items-center gap-4">
+          <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="name" className="text-black text-lg">Drug Type</Label>
+              <Select value={TypeofDrug} onValueChange={handleTypeOfDrug}>
                   <SelectTrigger className="w-full">
                     <SelectValue placeholder="Type of Drug" />
                   </SelectTrigger>
                   <SelectContent>
                     <SelectGroup>
+                      {/* <SelectLabel>What type is Drug?</SelectLabel> */}
                       <SelectItem value="liquid">Liquid</SelectItem>
                       <SelectItem value="capsule">Capsule</SelectItem>
                       <SelectItem value="tablet">Tablet</SelectItem>
                     </SelectGroup>
                   </SelectContent>
-                </Select>
-              </div>
-            </div>
+               </Select>
+          </div>
+          </div>
+          
+          </>
+        
+        )}
+        {count == 2 && (
+          <><div className="grid w-full items-center gap-4">
+          <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="name" className="text-black text-lg">Dispenser Serial Number</Label>
+              {SerialNumber?.length>0?(<Select value={SelectedSerialNumber} onValueChange={handleSelectedSerialNumber}>
+                  <SelectTrigger className="w-full">
+                    <SelectValue placeholder="Select" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      {/* <SelectLabel>What type is Drug?</SelectLabel> */}
+                      {SerialNumber?.map((no) => (
+                        <SelectItem key={no.serialNumber} value={no.serialNumber}>{no.serialNumber}</SelectItem>
+                      ))}
+                    </SelectGroup>
+                  </SelectContent>
+               </Select>):(<p className=''>Dispenser for provided description is not available. Please go back and reselect!</p>)}
+              
+          </div>
+          </div>
+          
           </>
         )}
-        {count === 2 && (
+                {count == 3 && (
           <>
-            <div className="grid w-full items-center gap-4">
-              <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="name" className="text-black text-lg">Dispenser Serial Number</Label>
-                {Array.isArray(SerialNumber) && SerialNumber.length > 0 ? (
-                  <Select value={SelectedSerialNumber} onValueChange={handleSelectedSerialNumber}>
-                    <SelectTrigger className="w-full">
-                      <SelectValue placeholder="Select" />
-                    </SelectTrigger>
-                    <SelectContent>
-                      <SelectGroup>
-                        {SerialNumber.map((no) => (
-                          <SelectItem key={no.serialNumber} value={no.serialNumber}>{no.serialNumber}</SelectItem>
-                        ))}
-                      </SelectGroup>
-                    </SelectContent>
-                  </Select>
-                ) : (
-                  <p className=''>Dispenser for provided description is not available. Please go back and reselect!</p>
-                )}
-              </div>
-            </div>
-          </>
-        )}
-        {count === 3 && (
-          <>
-            <p>Details of Medications</p>
+          <p>Details of Medications</p>
             <ScrollArea className="h-64">
-              {chunkArray(fields, 3).map((chunk, chunkIndex) => (
-                <div key={chunkIndex} className="field-group" style={{ marginBottom: '20px' }}>
-                  <button type='button' className='w-full text-white bg-gray-800 rounded-2xl p-3' onClick={() => toggleVisibility(chunkIndex)}>
-                    {visibleGroups[chunkIndex] ? '-' : '+'} Med {chunkIndex + 1}
-                  </button>
-                  {visibleGroups[chunkIndex] && (
-                    <ScrollArea>
-                      {chunk.map((field, index) => {
-                        const fieldTypes = ['Name', 'Interval', 'Dosage']
-                        return (
-                          <div key={index} style={{ marginBottom: '10px' }}>
-                            <label>
-                              <p className='my-2'>{fieldTypes[index % 3]}:</p>
-                              <input
-                                type="text"
-                                name={`${fieldTypes[index % 3]}`}
-                                className="InputClass"
-                                value={fields[index]}
-                                onChange={(e) => handleFieldChange(chunkIndex * 3 + index, e)}
-                                placeholder={`Enter ${fieldTypes[index % 3]}`}
-                              />
-                            </label>
-                          </div>
-                        )
-                      })}
-                    </ScrollArea>
-                  )}
-                </div>
-              ))}
+            {chunkArray(fields, 3).map((chunk, chunkIndex) => (
+  <div key={chunkIndex} className="field-group" style={{ marginBottom: '20px' }}>
+    <button type='button' className='w-full text-white bg-gray-800 rounded-2xl p-3' onClick={() => toggleVisibility(chunkIndex)}>
+      {visibleGroups[chunkIndex] ? '-' : '+'} Group {chunkIndex + 1}
+    </button>
+    {visibleGroups[chunkIndex] && (
+      <ScrollArea >
+        {chunk.map((field, index) => {
+          const fieldTypes = ['Name', 'Interval', 'Dosage'];
+          return (
+            <div key={index} style={{ marginBottom: '10px' }}>
+             
+              <label>
+                <p className='my-2'> {fieldTypes[index % 3]}:</p>
+               
+                <input
+                  type="text"
+                  name={`${fieldTypes[index % 3]}`}
+                  className="InputClass "
+                  value={fields[index]}
+                  onChange={(e) => handleFieldChange(chunkIndex * 3 + index, e)}
+                  placeholder={`Enter ${fieldTypes[index % 3]}`}
+                />
+              </label>
+            </div>
+          );
+        })}
+      </ScrollArea>
+    )}
+  </div>
+))}
             </ScrollArea>
+          
           </>
         )}
-        {count === 4 && (
-          <>
-            <div className="grid w-full items-center gap-4">
-              <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="name" className="text-black text-lg">Duration</Label>
-                <input className="InputClass" value={Duration} onChange={(e) => setDuration(e.target.value)} type="duration" name="duration" id="duration" placeholder="7" />
-              </div>
-            </div>
-            <div className="grid w-full items-center gap-4">
-              <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="name" className="text-black text-lg">Email</Label>
-                <input className="InputClass" value={email} onChange={(e) => setEmail(e.target.value)} type="email" name="email" id="email" placeholder="john@gmail.com" />
-              </div>
-            </div>
-            <div className="grid w-full items-center gap-4">
-              <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="name" className="text-black text-lg">Contact</Label>
-                <input className="InputClass" value={PhoneNum} onChange={(e) => setPhoneNum(e.target.value)} type="contact" name="contact" id="contact" placeholder="+234 07000 345672" />
-              </div>
-            </div>
-          </>
-        )}
-      </div>
 
-      <div className={`flex ${count > 1 ? "items-center justify-between" : "items-end justify-end"} mt-20`}>
+
+{count == 4 && (
+          <>
+          <div className="grid w-full items-center gap-4">
+          <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="name" className="text-black text-lg">Duration</Label>
+              <input className="InputClass " value={Duration} onChange={(e) => {setDuration(e.target.value)}}  type="duration" name="duration" id="duration" placeholder="7" />
+              
+          </div>
+          </div>
+
+          <div className="grid w-full items-center gap-4">
+          <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="name" className="text-black text-lg">Email</Label>
+              <input className="InputClass " value={email} onChange={(e) => {setEmail(e.target.value)}}   type="email" name="email" id="email" placeholder="john@gmail.com" />
+              
+          </div>
+          </div>
+          <div className="grid w-full items-center gap-4">
+          <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="name" className="text-black text-lg">Contact</Label>
+              <input className="InputClass " value={PhoneNum} onChange={(e) => {setPhoneNum(e.target.value)}}   type="contact" name="contact" id="contact" placeholder="+234 07000 345672" />
+              
+          </div>
+          </div>
+          </>
+        )}
+        </div>
+      
+
+        <div className={`flex ${count > 1 ? "items-center justify-between" : "items-end justify-end"} mt-20`}>
         {count > 1 && <Button onClick={handlePrevStep}>Back</Button>}
         {count === 4 ? <Button>Submit</Button> : (
           <Button onClick={handleNextStep}>
-            {loading ? <Circles height="20" width="20" color="white" ariaLabel="loading" /> : 'Next'}
+            {searching ? <Circles height="20" width="20" color="white" ariaLabel="searching" /> : 'Next'}
           </Button>
         )}
       </div>
+        {/* <div className={`flex  ${count> 1?"items-center justify-between":"items-end justify-end"}  mt-20`}>
+        
+          {count > 1  && (<Button onClick={handlePrevStep}>Back</Button>)}
+
+          {SerialNumber?.length > 0 &&(<Button className={`${count == 4?'hidden':'block'}`} onClick={handleNextStep}>Next</Button>)}
+          
+          {count == 1 &&(<Button className={`${SerialNumber?.length > 0?'hidden':'block'}`} onClick={handleNextStep}>Next</Button>)}
+          {count == 4&&<Button >Submit</Button>}
+      
+      </div> */}
     </div>
   )
 }
